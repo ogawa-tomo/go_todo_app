04534d977f1f693122fec03762a95576c5383d73
tomo <o_tomo_1990@yahoo.co.jp>
2026-06-30 21:11:29 +0900

    p.262

--- store/task_test.go
+++ store/task_test.go
@@ -10,19 +10,21 @@ import (
 	"github.com/ogawa-tomo/go_todo_app/clock"
 	"github.com/ogawa-tomo/go_todo_app/entity"
 	"github.com/ogawa-tomo/go_todo_app/testutil"
+	"github.com/ogawa-tomo/go_todo_app/testutil/fixture"
 )
 
 func TestRepository_ListTasks(t *testing.T) {
+	t.Parallel()
 	ctx := context.Background()
 	tx, err := testutil.OpenDBForTest(t).BeginTxx(ctx, nil)
 	t.Cleanup(func() { _ = tx.Rollback() })
 	if err != nil {
 		t.Fatal(err)
 	}
-	wants := prepareTasks(ctx, t, tx)
+	wantUserID, wants := prepareTasks(ctx, t, tx)
 
 	sut := &Repository{}
-	gots, err := sut.ListTasks(ctx, tx)
+	gots, err := sut.ListTasks(ctx, tx, wantUserID)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
@@ -61,37 +63,58 @@ func TestRepository_AddTask(t *testing.T) {
 	}
 }
 
-func prepareTasks(ctx context.Context, t *testing.T, con Execer) entity.Tasks {
+func prepareUser(ctx context.Context, t *testing.T, db Execer) entity.UserID {
 	t.Helper()
-
-	if _, err := con.ExecContext(ctx, "DELETE FROM task;"); err != nil {
-		t.Logf("failed to initialize task: %v", err)
+	u := fixture.User(nil)
+	result, err := db.ExecContext(ctx, "insert into user (name, password, role, created, modified) values (?, ?, ?, ?, ?);", u.Name, u.Password, u.Role, u.Created, u.Modified)
+	if err != nil {
+		t.Fatalf("insert user: %v", err)
 	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		t.Fatalf("got user_id: %v", err)
+	}
+	return entity.UserID(id)
+}
+
+func prepareTasks(ctx context.Context, t *testing.T, con Execer) (entity.UserID, entity.Tasks) {
+	t.Helper()
+	userID := prepareUser(ctx, t, con)
+	otherUserID := prepareUser(ctx, t, con)
 	c := clock.FixedClocker{}
 	wants := entity.Tasks{
 		{
-			Title: "want task 1", Status: "todo",
+			UserID: userID,
+			Title:  "want task 1", Status: "todo",
 			Created: c.Now(), Modified: c.Now(),
 		},
 		{
-			Title: "want task 2", Status: "todo",
-			Created: c.Now(), Modified: c.Now(),
-		},
-		{
-			Title: "want task 3", Status: "done",
+			UserID: userID,
+			Title:  "want task 2", Status: "todo",
 			Created: c.Now(), Modified: c.Now(),
 		},
 	}
+	tasks := entity.Tasks{
+		wants[0],
+		{
+			UserID:   otherUserID,
+			Title:    "not want task",
+			Status:   "todo",
+			Created:  c.Now(),
+			Modified: c.Now(),
+		},
+		wants[1],
+	}
 	result, err := con.ExecContext(
 		ctx,
-		`Insert into task (title, status, created, modified)
+		`Insert into task (user_id, title, status, created, modified)
 			values
-				(?, ?, ?, ?),
-				(?, ?, ?, ?)
-				(?, ?, ?, ?);`,
-		wants[0].Title, wants[0].Status, wants[0].Created, wants[0].Modified,
-		wants[1].Title, wants[1].Status, wants[1].Created, wants[1].Modified,
-		wants[2].Title, wants[2].Status, wants[2].Created, wants[2].Modified,
+				(?, ?, ?, ?, ?),
+				(?, ?, ?, ?, ?)
+				(?, ?, ?, ?, ?);`,
+		tasks[0].UserID, tasks[0].Title, tasks[0].Status, tasks[0].Created, tasks[0].Modified,
+		tasks[1].UserID, tasks[1].Title, tasks[1].Status, tasks[1].Created, tasks[1].Modified,
+		tasks[2].UserID, tasks[2].Title, tasks[2].Status, tasks[2].Created, tasks[2].Modified,
 	)
 	if err != nil {
 		t.Fatal(err)
@@ -100,8 +123,8 @@ func prepareTasks(ctx context.Context, t *testing.T, con Execer) entity.Tasks {
 	if err != nil {
 		t.Fatal(err)
 	}
-	wants[0].ID = entity.TaskID(id)
-	wants[1].ID = entity.TaskID(id + 1)
-	wants[2].ID = entity.TaskID(id + 2)
-	return wants
+	tasks[0].ID = entity.TaskID(id)
+	tasks[1].ID = entity.TaskID(id + 1)
+	tasks[2].ID = entity.TaskID(id + 2)
+	return userID, wants
 }
